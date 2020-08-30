5e0ef55abc416274133dc90bcf2bc144baf8afe6
Josh <zylo.codes@gmail.com>
2020-08-30 07:42:18 -0500

    Updates readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This plugin renders the rwm-audioplayer-vue widget by using a WP shortcode.
 
 After installing the plugin, use the following shortcode:
 
-`[rwm-audioplayer slug="sg" color="orange" autoplay="true" playlist="true" rwmlink="true"]`
+`[rwm-audioplayer slug="swansonfinancial" color="orange" autoplay="true" playlist="true" rwmlink="true"]`
--- realwealthmedia-audioplayer.php
+++ realwealthmedia-audioplayer.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: <em>Real</em> Wealth<sup>&reg;</sup> Media Audio Player
-  Plugin URI:  https://realwealthmarketing.com/podcast-player
+  Plugin URI:  https://github.com/8ctopotamus/rwm-audioplayer-wp
   Description: Embed the <em>Real</em> Wealth<sup>&reg;</sup> Media Audio Player on your site!
   Version:     1.0
   Author:      Zylo, LLC
@@ -14,17 +14,17 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 // Register all scripts and styles.
 function rwm_audioplayer_styles_scripts() {
-	wp_register_style('rwm-audioplayer-styles', 'https://realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.css');
-  wp_register_script('rwm-audioplayer-js', 'https://realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.js', '', '', true );
-  wp_register_script('rwm-detect-ie-js', 'https://realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/detect-ie.js', '', '', true );
+	wp_register_style('rwm-audioplayer-styles', '//realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.css');
+  wp_register_script('rwm-audioplayer-js', '//realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.js', '', '', true );
+  wp_register_script('rwm-detect-ie-js', '//realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/detect-ie.js', '', '', true );
 }
 add_action('wp_loaded', 'rwm_audioplayer_styles_scripts' );
 
 
 // add preload links to <head>
 function rwm_audioplayer_preload_links() { ?>
-  <link as="style" href="https://realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.css" rel="preload" />
-  <link as="script" href="https://realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.js" rel="preload" />
+  <link as="style" href="//realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.css" rel="preload" />
+  <link as="script" href="//realwealthmediahttpspullzone-realwealthradiol.netdna-ssl.com/embeds/podcastplayer/v1/app.js" rel="preload" />
 <?php }
 add_action('wp_head', 'rwm_audioplayer_preload_links');
 
@@ -32,7 +32,7 @@ add_action('wp_head', 'rwm_audioplayer_preload_links');
 // shortcode
 function rwm_audioplayer_shortcode_func( $atts ) {
   if (empty($atts['slug'])) {
-    return 'You need to add your slug to render the audioplayer. Please see <a href="https://realwealthmarketing.com/podcast-player/" target="_blank">https://realwealthmarketing.com/podcast-player</a> for instructions.';
+    return 'You need to add your slug to render the audioplayer. Please see <a href="https://realwealthmarketing.com/podcast-player/" target="_blank">https//realwealthmarketing.com/podcast-player</a> for instructions.';
   }
 	wp_enqueue_style('rwm-audioplayer-styles');
   wp_enqueue_script('rwm-audioplayer-js');
